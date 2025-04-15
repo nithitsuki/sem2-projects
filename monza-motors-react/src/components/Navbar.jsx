eb6df6e3e4f050e7702a77fb1bb1755b4fe391ab
nithitsuki <nithilanr@gmail.com>
2025-04-15 18:53:15 +0530

    removed mclaren from navbar

--- monza-motors-react/src/components/Navbar.jsx
+++ monza-motors-react/src/components/Navbar.jsx
@@ -20,9 +20,6 @@ function Navbar() {
                 <Link className="nav-link" to="/cars">Cars</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/carpages/p1">Mclaren</Link> {/* Link to p1.jsx */}
-              </li>
-              <li className="nav-item">
                 <Link className="nav-link" to="/customizer">Customizer</Link>
               </li>
               <li className="nav-item">
